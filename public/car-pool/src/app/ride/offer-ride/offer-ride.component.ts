4d948ae212f2976d9e49dddd9b3de8c05e70ef8d
santoshwalke <santoshw143@gmail.com>
2019-04-02 11:10:14 +0530

    reactive from validation

--- public/car-pool/src/app/ride/offer-ride/offer-ride.component.ts
+++ public/car-pool/src/app/ride/offer-ride/offer-ride.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+//import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-offer-ride',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offer-ride.component.css']
 })
 export class OfferRideComponent implements OnInit {
-
+  offerForm: FormGroup;
+  
   constructor() { }
 
   ngOnInit() {
+    this.offerForm = new FormGroup({
+      'txtName': new FormControl('', [Validators.required]),
+      'txtStart': new FormControl('',[Validators.required]), 
+      'txtDestination': new FormControl('',[Validators.required]),
+      'txtCar': new FormControl('',[Validators.required]),
+      'txtSeat':  new FormControl('',[Validators.required, Validators.min(0), Validators.max(8)])
+    })
+  }
+  
+  onSubmit() {
+    console.log(this.offerForm);
+  }
+
+  goBack() {
+    //this.location.back();
+    //console.log(this.location);
   }
 
 }
