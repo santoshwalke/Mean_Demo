ca0d7ac154836cfe448220e089e111c0ef811b0e
santoshwalke <santoshw143@gmail.com>
2019-04-03 10:59:35 +0530

    cancel ride comp

---
+++ public/car-pool/src/app/ride/cancel-ride/cancel-ride.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { RideDataService } from '../ride-data.service';
+
+@Component({
+  selector: 'app-cancel-ride',
+  templateUrl: './book-ride.component.html',
+  styleUrls: ['./book-ride.component.css']
+})
+export class CancelRideComponent implements OnInit {
+
+  constructor(private router: Router, private rideDataService: RideDataService) { }
+  bookRideDetails = {};
+  
+  ngOnInit() {
+  }
+
+  showAllRide() {
+    this.rideDataService.fetchAllRides();
+    this.router.navigate(['show_ride']);
+  }
+
+  offerRide() {
+    this.router.navigate(['offer_ride']);
+  }
+
+}
