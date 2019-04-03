f5bb468fe30c67a6a0d1f89207ec6121f8b9e529
santoshwalke <santoshw143@gmail.com>
2019-04-03 10:08:02 +0530

    book & offer ride updated

--- public/car-pool/src/app/ride/ride.service.ts
+++ public/car-pool/src/app/ride/ride.service.ts
@@ -7,10 +7,20 @@ import { Ride } from './ride.model';
 })
 export class RideService {
     rideChanged = new Subject<Ride[]>();
-
+    bookRideChanged = new Subject<{id: number, rideData: object, message: string}>();
+    offerRideResponseChanged = new Subject<string>();
+    
   constructor() { }
 
   getRide(rides: Ride[]) {
       this.rideChanged.next(rides.slice());
+  }
+  
+  getBookRideDetails(rides: {id: number, rideData: object, message: string}) {
+    this.bookRideChanged.next(rides.slice());
+  }
+  
+  offerRideResponse(data: {message: string}) {
+    this.offerRideResponseChanged.next(data.message);
   }
 }
